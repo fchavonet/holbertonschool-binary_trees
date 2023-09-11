adc72ccbca0ad353b4ec3dc47db6db80ce5ed640
Pandolowitz <pandolowitz@gmail.com>
2023-09-11 14:44:12 +0200

    Creation of the binary_tree_postorder function.

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a binary tree
- *                       using in-order traversal.
+ * binary_tree_postorder - Goes through a binary tree
+ *                       using post-order traversal.
  *
  * @tree: pointer to the root node of the tree to traverse.
  * @func: pointer to a function to call for each node.
  *        The value in the node must be passed as a parameter to this function.
  */
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if ((tree == NULL) || (func == NULL))
 		return;
 
-	binary_tree_inorder(tree->left, func);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
 	func(tree->n);
-	binary_tree_inorder(tree->right, func);
 }
