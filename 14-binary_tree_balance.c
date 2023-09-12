dbb6324e49250a373c298e03cf5539d6ddeac19e
SpStigma <githubstigma@gmail.com>
2023-09-12 11:28:16 +0200

    Fix: retrun 0 if tree is NULL.

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,5 +33,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
+
 	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 }
