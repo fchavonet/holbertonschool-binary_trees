2e48ef097c353eab62cb52df4040ec4a7f84cc2b
SpStigma <githubstigma@gmail.com>
2023-09-12 11:24:39 +0200

    Creation of the binary_tree_balance function.

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - Measures the height of a binary tree.
+ *
+ * @tree: pointer to the root node of the tree to measure the height.
+ *
+ * Return: the height of a binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree.
+ *
+ * @tree: pointer to the root node of the tree to measure the balance factor.
+ *
+ * Return: the balance factor.
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+}
